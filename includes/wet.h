012d8d9201de16b22b9411ecc31e402ffff456b7
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-12 00:36:18 -0300

    feat: macros de plataforma adicionadas

--- includes/wet.h
+++ includes/wet.h
@@ -5,6 +5,17 @@
 #ifndef WET_H
 #define WET_H
 
+// Pegando a plataforma alvo e definindo as macros de plataforma
+#if defined(_WIN32) || defined(_WIN64)          // Windows
+    #define WET_PLATFORM_WINDOWS            
+#elif defined(__APPLE__) || defined(__MACH__)   // MacOs
+    #define WET_PLATFORM_MACOS
+#elif defined(__linux__)                        // Linux
+    #define WET_PLATFORM_LINUX 
+#else
+    // Plataforma não suportada
+#endif
+
 
 
 #endif

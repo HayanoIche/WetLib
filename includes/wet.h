c20eb893ef432466e1f54cb7a5e79bd617618f7e
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-12 00:52:38 -0300

    feat: definindo apelidos pra tipos específicos
    renomeando os intX_t pra não conter o t, e o float e double pra float32 e float64

--- includes/wet.h
+++ includes/wet.h
@@ -5,7 +5,13 @@
 #ifndef WET_H
 #define WET_H
 
-// Pegando a plataforma alvo e definindo as macros de plataforma
+#include <stdint.h>
+
+
+//
+//  Pegando a plataforma alvo e definindo as macros de plataforma
+//
+
 #if defined(_WIN32) || defined(_WIN64)          // Windows
     #define WET_PLATFORM_WINDOWS            
 #elif defined(__APPLE__) || defined(__MACH__)   // MacOs
@@ -16,6 +22,22 @@
     // Plataforma não suportada
 #endif
 
+
+//
+//  Renomeando tipos específicos para algo mais simples e utilizável
+//
+typedef int8_t   int8;
+typedef int16_t  int16;
+typedef int32_t  int32;
+typedef int64_t  int64;
+
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float    float32;
+typedef double   float64;
 
 
 #endif

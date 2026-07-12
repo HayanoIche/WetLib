11a1769243f47ce40f0ed96d77fc78bdf45bdbc8
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-12 01:16:11 -0300

    feat: structs basicas: vec2, vec3, rect e color
    estruturas básicas pro vetor 2D, vetor 3D, retangulo (4 vertices) e para cor adicionados

--- includes/wet.h
+++ includes/wet.h
@@ -5,12 +5,13 @@
 #ifndef WET_H
 #define WET_H
 
+
 #include <stdint.h>
 
 
-//
+// ----------------------------------------------------------------------
 //  Pegando a plataforma alvo e definindo as macros de plataforma
-//
+// ----------------------------------------------------------------------
 
 #if defined(_WIN32) || defined(_WIN64)          // Windows
     #define WET_PLATFORM_WINDOWS            
@@ -23,9 +24,10 @@
 #endif
 
 
-//
+// ----------------------------------------------------------------------
 //  Renomeando tipos específicos para algo mais simples e utilizável
-//
+// ----------------------------------------------------------------------
+
 typedef int8_t   int8;
 typedef int16_t  int16;
 typedef int32_t  int32;
@@ -38,6 +40,58 @@ typedef uint64_t uint64;
 
 typedef float    float32;
 typedef double   float64;
+
+// ----------------------------------------------------------------------
+//  Structs padrões da biblioteca
+// ----------------------------------------------------------------------
+
+// Struct para vetores bidimensionais
+typedef struct {
+    float32 x;
+    float32 y;
+} Vec2;
+
+typedef struct {
+    int32 x;
+    int32 y;
+} Vec2i;
+
+// Struct para vetores tridimensional
+typedef struct {
+    float32 x;
+    float32 y;
+    float32 z;
+} Vec3;
+
+typedef struct {
+    float32 x;
+    float32 y;
+    float32 z;
+} Vec3i;
+
+// Struct para retangulos
+typedef struct {
+    float32 x1;
+    float32 y1;
+    float32 x2;
+    float32 y2;
+} Rect;
+
+typedef struct {
+    int32 x1;
+    int32 y1;
+    int32 x2;
+    int32 y2;
+} Recti;
+
+// Struct para cores
+typedef struct {
+    uint8 r;
+    uint8 g;
+    uint8 b;
+    uint8 a;
+} Color;
+
 
 
 #endif

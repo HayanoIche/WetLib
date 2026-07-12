5b8fe72b4495a1db7b55952e6a223e8ceb776658
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-12 19:52:04 -0300

    feat: funções da janela básicas (ciclo de vida)
    Funções window_create, window_update, window_destroy adicionadas
    Struct pra configuração inicial da janela também

--- includes/wet/window.h
+++ includes/wet/window.h
@@ -6,11 +6,40 @@
 #ifndef WET_WINDOW_H
 #define WET_WINDOW_H
 
+#include "wet.h"
+
+// Estrutura básica de configuração da janela
+typedef struct {
+    uint8 width;
+    uint8 height;
+    const char* title;
+    bool fullscreen;
+} WindowConfig;
 
 
+// ----------------------------------------------------------------------
+//  Funções publicas pro uso da janela
+// ----------------------------------------------------------------------
 
+// Ciclo de vida da janela
+bool window_create(WindowConfig config);
+void window_update(void);
+void window_destroy(void);
 
+// Outros
+bool window_should_close(void);
 
-
+//
+//      A implementar no futuro:
+//
+//  void window_set_position(uint8 x, uint8 y);
+//  void window_set_size(uint8 width, uint8 height);
+//  void window_set_fullscreen(bool mode);
+//  void window_set_borderless(bool mode);
+//  void window_set_resizeable(bool mode);
+//  void window_set_caption(const char* title);
+//
+//  também os getters desses setters
+//
 
 #endif

7e18426c5520c6ce2d59b69b87c88bf5e29ba292
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-13 18:00:01 -0300

    feat: header do sistema de tempo (fps e deltatime)
    Funções do ciclo de vida do sistema declaradas, uma função pra setar o fps ideal do jogo e funções pra pegar o fps e o delta time

---
+++ includes/wet/time.h
@@ -0,0 +1,27 @@
+//
+//  Arquivo pertencente de um sub-header da WetLib
+//  feito por Hayano
+//
+
+#ifndef WET_TIME_H
+#define WET_TIME_H
+
+#include "wet.h"
+
+// Ciclo de vida do sistema da contagem do tempo
+void time_init(void);
+void time_update(void);
+void time_shut(void);
+
+// Configurador do FPS
+void fps_set_target(uint16 fps);
+
+uint16 fps_get_target(void);
+uint16 fps_get(void);
+uint16 fps_real_get(void);
+
+float64 delta_time_get(void); // Pra pegar o deltatime
+
+#define delta_time time_get_delta() // Macro pra pegar o delta_time
+
+#endif

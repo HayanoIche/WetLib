e59f5138be1d3d0eae91a08a9e2fec3c02479438
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-13 18:37:29 -0300

    feat: funções auxiliares pro sistema de tempo

--- src/core/time.c
+++ src/core/time.c
@@ -29,4 +29,66 @@ typedef struct {
 
 static TimeManager time_manager = { 0 };
 
+// ----------------------------------------------------------------------
+//  Funções Auxiliares
+// ----------------------------------------------------------------------
+
+//
+//    Essa função retorna o tempo total de segundos
+//  a partir de um momento mágico (Ex.: quando o pc ligou)
+//
+//      Ela serve basicamente para só medir intervalos
+//  de tempo, ai ela se baseia a partir desse momento mágico
+//
+// - Hayano
+//
+
+static float64 get_time_in_seconds(void)
+{
+    #if defined(WET_PLATFORM_WINDOWS)
+        static LARGE_INTEGER frequency; // Tipo da propria microsoft (mas é basicamente um int64)
+        static bool has_quad = false;
+
+        if (!has_quad) // Para só calcular a frequência uma vez
+        {
+            QueryPerformanceFrequency(&frequency);
+            has_quad = true;
+        }
+        
+        LARGE_INTEGER counter;
+        QueryPerformanceCounter(&counter);
+        return (float64)counter.QuadPart / (float64)frequency.QuadPart;
+
+    #else // No macos ou no linux
+        struct timespec ts;
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+        return (float64)ts.tv_sec + (float64)ts.tv_nsec / 1000000000.0;
+        
+    #endif
+}
+
+//
+//      Função que serve para "dormir" o programa,
+//  pra economizar os frames "a mais" que o processador
+//  consegue calcular mas não precisa.
+//  
+//    Se deixar o programa sem limitar os frames por
+//  segundo ele pega o máximo da cpu possível pra
+//  rodar o máximo de loops possível
+//
+//  (meu pc faz um barulho estranho quando isso ocorre inclusive)
+//
+//  - Hayano
+//
+
+static void wait_time(double seconds)
+{
+    if (seconds <= 0.0) return;
+
+    #if defined(WET_PLATFORM_WINDOWS)
+        Sleep((DWORD)(seconds * 1000.0)); // Dormir no windows
+    #else
+        usleep((useconds_t)(seconds * 1000000.0));  // Dormir no linux/macos
+    #endif
+}
 

a0b831f0c3010ae15db4f7ea120bacc9d828e630
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-14 16:54:58 -0300

    feat: verificação de segurança na função de compilar shaders do opengl

--- src/platforms/gfx/opengl.c
+++ src/platforms/gfx/opengl.c
@@ -106,9 +106,48 @@ static const char* default_fragment_sh_source =
 static uint32 compile_shader(uint32 type, const char* source)
 {
     // Compilando o shaders
-    unit32 id = glCreateShader(type);       // Criando ele na GPU
-    glShaderSource(id, 1, &source, NULL);   // Embutindo a string do shaders dentro do shader da GPU
-    glCompileShader(id);                    // Compila o shader em si
+    {
+        unit32 id = glCreateShader(type);       // Criando ele na GPU
+        glShaderSource(id, 1, &source, NULL);   // Embutindo a string do shaders dentro do shader da GPU
+        glCompileShader(id);                    // Compila o shader em si
+    }
 
+    // Checando se ouve algum erro na compilação do shader
+    {
+        int32 result;
+
+        //  nota:
+        //  glGetShaderiv()
+        //  
+        //      Essa função funciona buscando informações internas do shader
+        //  nesse caso o parametro de COMPILE STATUS responde se ele ta compilado
+        //  certinho (GL_TRUE) ou não (GL_FALSE)
+        //
+        //  - Hayano
+        //
+
+        glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+        if (result==GL_FALSE)
+        {
+            // Pegando a mensagem de erro
+            int length;
+            char message[512];
+
+            glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+            glGetShaderInfoLog(id, length, &length, message);
+
+            LOG_ERROR("[OPEN GL] Falha ao compilar um shader...\n%s\n", message);
+
+            glDeleteShader(id);
+            return 0;
+        }
+        else
+        {
+            LOG_INFO("[OPEN GL] Shader compilado com sucesso!");
+        }
+    }
+
+    // Retornando o shaders
     return id;
 }

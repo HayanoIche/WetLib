fea986db560ad191a2157575118f4702a383c079
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-07-29 13:04:41 -0300

    feat: resto da implementação da criação de surface no openGL

--- src/platforms/renderer/openGL/opengl_surface.c
+++ src/platforms/renderer/openGL/opengl_surface.c
@@ -46,4 +46,38 @@ bool opengl_surface_create(const char* name, uint32 width, uint32 height)
     strncpy(surface->name, name, sizeof(surface->name) - 1);
     surface->width = width;
     surface->height = height;
+
+    // Iniciando o FrameBuffer
+    glGenFramebuffers(1, &surface->fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, surface->fbo);
+
+    // Criando a textura
+    glGenTextures(1, &surface->texture);
+    glBindTexture(GL_TEXTURE_2D, surface->texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, surface->texture, 0);
+
+    // Renderbuffer para Depth
+    glGenRenderbuffers(1, &surface->rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, surface->rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, surface->rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        LOG_ERROR("[OPEN GL] Falha ao validar o framebuffer da surface '%s'", surface->name);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        return false;
+    }
+
+    surface->active = true;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    return true;
 }
